68871d7171c0ca859bae15aa8e0df33333b6ee61
Mauro Chojrin <mauro.chojrin@codurance.com>
2023-10-09 15:45:41 +0200

    Adding support for Maven

--- hole1/src/test/java/hole1/TakeHomeCalculatorTest.java
+++ hole1/src/test/java/hole1/TakeHomeCalculatorTest.java
@@ -2,13 +2,13 @@ package hole1;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.TestCase.assertEquals;
 
 public class TakeHomeCalculatorTest {
 
     @Test
     public void canCalculateTax() throws Exception {
-        Integer first = new TakeHomeCalculator(10).netAmount(new TakeHomeCalculator.Pair<>(40, "GBP"), new TakeHomeCalculator.Pair<>(50, "GBP"), new TakeHomeCalculator.Pair<>(60, "GBP")).first;
+        Integer first = new TakeHomeCalculator(10).netAmount(new hole1.TakeHomeCalculator.Pair<>(40, "GBP"), new TakeHomeCalculator.Pair<>(50, "GBP"), new TakeHomeCalculator.Pair<>(60, "GBP")).first;
         assertEquals(135, first.intValue());
     }
 
